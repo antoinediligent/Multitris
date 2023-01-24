0712755a4035031a1f96b2617f9864305ce4a83d
antoine.diligent <antoine.diligent@gmail.com>
2023-01-24 23:11:11 +0100

    Menus and move scripts to new folder

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame(int nbPlayers)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        // This line because Application.Quit() is not going to shutdown the game inside the Unity editor
+        // at least we can see in the logs that it's working
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
